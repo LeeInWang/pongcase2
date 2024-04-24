4ce9f59a82a05d7a5afdacee9516437fe6e1b5f4
Inwang <https://github.com/LeeInWang/Easy_to_manage.git>
2024-04-24 21:02:47 +0900

    0424final

---
+++ pongcase/src/main/resources/static/js/sjheader.js
@@ -0,0 +1,8 @@
+
+    <!-- 메뉴 닫히는 기능 -->
+    function toggleSidebar() {
+    const menu = document.querySelector(".menu");
+    const sidebar = document.getElementById("sidebarsj");
+    menu.classList.toggle("active");
+    sidebar.classList.toggle("active");
+}
